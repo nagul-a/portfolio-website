6a9b8224ed9e2bd523023bb8a8d364cca9acac37
nagul-a <nagulkdm@gmail.com>
2025-10-14 18:08:25 +0530

    Update portfolio project with new styles and content

--- portfolio-react/src/Projects.js
+++ portfolio-react/src/Projects.js
@@ -18,6 +18,18 @@ function Projects() {
           <h3>Portfolio Website</h3>
           <p>This very site! Built using React.js to showcase my journey, projects, and contact info.</p>
         </div>
+        <div className="project-card">
+          <h3>Smart Grocerry Tracker</h3>
+          <p>Smart Grocery Tracker is a digital tool or app designed to help users efficiently manage their grocery shopping and pantry inventory.</p>
+        </div>
+        <div className="project-card">
+          <h3>PotHole Detector</h3>
+          <p>Pothole Detector is a smart system designed to identify and report road surface damage, especially potholes, using technology like sensors, cameras, and AI.</p>
+        </div>
+        <div className="project-card">
+          <h3>Crack Jack</h3>
+          <p>Crack Jack is a Java-based encryption and decryption tool that goes beyond the classic Caesar cipher. It integrates advanced cryptographic algorithms like AES and RSA to secure messages with modern standards.</p>
+        </div>
       </div>
     </section>
   );
